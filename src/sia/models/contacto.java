d7390c7bb882f03bc98841bad2b751d77eb62cc1
Hanna Garcia Guerra <hnngarcia11@gmail.com>
2025-08-14 18:21:21 -0500

    Estructura clase contacto

--- src/sia/models/contacto.java
+++ src/sia/models/contacto.java
@@ -1,5 +1,52 @@
 package sia.models;
 
 public class contacto {
+    //Atributos
+    private String nombre;
+    private String correo;
+    private String asunto;
+    private String mensaje;
+
+    //Constructor
+    public contacto(String nombre, String correo, String asunto, String mensaje){
+        this.nombre = nombre;
+        this.correo = correo;
+        this.asunto = asunto;
+        this.mensaje = mensaje;
+    }
+
+    //Setters
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
+
+    public void setAsunto(String asunto){
+        this.asunto = asunto;
+    }
+
+    public void setMensaje(String mensaje){
+        this.mensaje = mensaje;
+    }
+
+    //Getters
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getCorreo(){
+        return correo;
+    }
+
+    public String getAsunto(){
+        return asunto;
+    }
+
+    public String getMensaje(){
+        return mensaje;
+    }
     
 }
