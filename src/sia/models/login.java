1b6c0ba7ba61f585335b4511fd105ec389a8ea35
Hanna Garcia Guerra <hnngarcia11@gmail.com>
2025-08-14 18:39:10 -0500

    Estructura clase login

--- src/sia/models/login.java
+++ src/sia/models/login.java
@@ -1,5 +1,34 @@
 package sia.models;
 
 public class login {
+    //Atributos
+    private String correo;
+    private String contraseña;
+
+    //Constructor
+    public login(String correo, String contraseña){
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+    //Setters
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
+
+    public void setContraseña(String contraseña){
+        this.contraseña = contraseña;
+    }
+
+    //Getters
+    public String getCorreo(){
+        return correo;
+    }
+
+    public String getContraseña(){
+        return contraseña;
+    }
+
+
     
 }
