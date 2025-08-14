00b5cb518172127ab1a17f2ba51b941e3d48c668
Hanna Garcia Guerra <hnngarcia11@gmail.com>
2025-08-14 18:07:13 -0500

    Estructura clase estudiante

--- src/sia/models/estudiante.java
+++ src/sia/models/estudiante.java
@@ -1,5 +1,52 @@
 package sia.models;
 
 public class estudiante {
+    //Atributos
+    private int id;
+    private String nombre;
+    private String curso;
+    private String contacto;
+
+    //Constructor
+    public estudiante(int id, String nombre, String curso, String contacto){
+        this.id = id;
+        this.nombre = nombre;
+        this.curso = curso;
+        this.contacto = contacto;
+    }
+
+    //Setters
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setCurso(String curso){
+        this.curso = curso;
+    }
+
+    public void setContacto(String contacto){
+        this.contacto = contacto;
+    }
+
+    //Getters
+    public int getId(){
+        return id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getCurso(){
+        return curso;
+    }
+
+    public String getContacto(){
+        return contacto;
+    }
     
 }
