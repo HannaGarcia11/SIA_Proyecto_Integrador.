1c603295f2707c844787794057700d1f6f1d5b20
Hanna Garcia Guerra <hnngarcia11@gmail.com>
2025-08-14 18:55:32 -0500

    Estructura clase nosotros

--- src/sia/models/nosotros.java
+++ src/sia/models/nosotros.java
@@ -1,5 +1,53 @@
 package sia.models;
 
 public class nosotros {
+    //Atrubutos
+    private String nombreSistema;
+    private String mision;
+    private String vision;
+    private String descripcionSistema;
+
+    //Constructor
+    public nosotros(String nombreSistema, String mision, String vision, String descripcionSistema){
+        this.nombreSistema = nombreSistema;
+        this.mision = mision;
+        this.vision = vision;
+        this.descripcionSistema = descripcionSistema;
+    }
+
+    //Setters
+    public void setNombreSistema(String nombreSistema){
+        this.nombreSistema = nombreSistema;
+    }
+
+    public void setMision(String mision){
+        this.mision = mision;
+    }
+
+    public void setVision(String vision){
+        this.vision = vision;
+    }
+
+    public void setDescripcionSistema(String descripcionSistema){
+        this.descripcionSistema = descripcionSistema;
+    }
+
+    //Getters
+    public String getNombreSistema(){
+        return nombreSistema;
+    }
+
+    public String getMision(){
+        return mision;
+    }
+
+    public String getVision(){
+        return vision;
+    }
+
+    public String getDescripcionSistema(){
+        return descripcionSistema;
+    }
+    
     
 }
